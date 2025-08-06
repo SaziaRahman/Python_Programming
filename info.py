fe2b2cd2a91415bcbaee5017e68e33c3596d4c53
SaziaRahman <saziarahman2000@gmail.com>
2025-08-07 01:41:04 +0600

    Python Programming

--- info.py
+++ info.py
@@ -2,8 +2,18 @@ import csv
 
 with open("info.csv", "r") as file:
     lines = csv.DictReader(file)
+    totalStudents = {}
     for row in lines:
         name = row["Name"] 
         course = row["Course"]
         roll = row["Roll"]
         print(f"Student, {name} having roll no. {roll}, has taken {course}")
+        if course in totalStudents:
+            totalStudents[course][course] += {name}
+        else:
+            totalStudents[course] = {course: [name]}
+
+
+for course in totalStudents:
+    print(f"{course}: {totalStudents[course]}")
+
