d6aa59156870eac39fd16f4db6d3073f2ba739ef
SaziaRahman <saziarahman2000@gmail.com>
2025-08-08 07:01:30 +0600

    Python Programming

--- info_db.py
+++ info_db.py
@@ -1,11 +1,12 @@
 import sqlite3
 
 db = sqlite3.connect("info.db")
+conn = db.cursor()
 
 course = input("Course: ")
 
 db.commit()
-query = db.execute("SELECT Name FROM info WHERE Course= '"+ course +"'")
+query = conn.execute("SELECT * FROM info WHERE Course = ?", (course,))
 
 
 for line in query:
